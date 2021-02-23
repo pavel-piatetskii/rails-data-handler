4920a121b05e48b7268aacfaaf0a88abbe0423a6
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-02-23 12:38:38 -0800

    Add jQuery logic to switch between message and info

--- app/assets/javascripts/update.js
+++ app/assets/javascripts/update.js
@@ -1,5 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(() => {
-  console.log('It\'s alive!')
+  $('.updates-feed__container').on('click', (e) => {
+    $(e.currentTarget).children('.updates-feed__container__message').toggle()
+    $(e.currentTarget).children('.updates-feed__container__details').toggle()
+  })
 })
