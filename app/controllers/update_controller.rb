c743ba16374fa63363f4eb19367bd29f47275b74
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-02-23 09:53:49 -0800

    Implement getting data set from the external URL

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -1,4 +1,15 @@
 class UpdateController < ApplicationController
+
+  require 'open-uri'
+  require 'json'
+
   def index
+
+    url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
+    json_from_url = open(url).read
+    updates_all = JSON.parse(json_from_url)
+    #byebug
+
   end
+
 end
