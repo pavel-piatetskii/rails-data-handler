61aa504da96fd3a62300e62fad23637abd9260b3
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-02-23 09:59:20 -0800

    Add saving incoming updates to the DB

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -8,8 +8,14 @@ class UpdateController < ApplicationController
     url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
     json_from_url = open(url).read
     updates_all = JSON.parse(json_from_url)
-    #byebug
-
+    
+    updates_all.map {|update|
+      update[:external_id] = update.delete :id
+      Update.create(
+        update
+        )}
+        
+    byebug
   end
 
 end
