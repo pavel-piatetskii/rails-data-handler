2558b3dc0d8d8d81455473d1dafd2b5776aeddde
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-02-23 10:21:04 -0800

    Fix JSON parser to symbolize keys

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -7,11 +7,12 @@ class UpdateController < ApplicationController
 
     url = "https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json"
     json_from_url = open(url).read
-    updates_all = JSON.parse(json_from_url)
+    updates_all = JSON.parse(json_from_url, opt = {symbolize_names: true})
     
+    #existing_external_ids = Update.pluck(:external_id)
     updates_all.map {|update|
-      update[:external_id] = update.delete 'id'
-      #byebug
+      byebug
+      update['external_id'] = update.delete 'id'
       Update.create(
         update
         )}
